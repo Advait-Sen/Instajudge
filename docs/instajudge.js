0a4d8da76e3f90e966e2b89e1a1fd1fda07c9d13
Advait-Sen <advaitsen78@gmail.com>
2021-03-19 11:02:23 +0100

    Making dynamic display to do multiple things at once

--- docs/instajudge.js
+++ docs/instajudge.js
@@ -1,73 +1,163 @@
 //Instajudge app
 
-var speakers = new Map([
-    ["PM", ["Prime Minister",0,[]]],
-    ["DP", ["Deputy Prime Minister", 0, []]],
-    ["GB", ["Member of Government Benches", 0, []]],
-    ["GW", ["Government Whip", 0, []]],
-    ["OL", ["Opposition Leader", 0, []]],
-    ["DO", ["Deputy Opposition Leader", 0, []]],
-    ["OB", ["Member of Opposition Benches", 0, []]],
-    ["OW", ["Opposition Whip", 0, []]]
-]);
+//Instajudge app
 
-var prev = ['', '', true];
+var Speakers={
+    'PM' : {
+        'name':'Prime Minister',
+        'code':'PM',
+        'score':0,
+        'team':"First Government",
+        'points':{},
+        'pressed':false,
+        'default_colour':'btn btn-primary'
+    },
+
+    'DP' : {
+        'name':'Deputy Prime Minister',
+        'code':'DP',
+        'score':0,
+        'team':"First Government",
+        'points':{},
+        'pressed':false,
+        'default_colour':'btn btn-primary'
+    },
+
+    'GB' : {
+        'name':'Member of Government Benches',
+        'code':'GB',
+        'score':0,
+        'team':"Second Government",
+        'points':{},
+        'pressed':false,
+        'default_colour':'btn btn-primary'
+    },
+
+    'GW' : {
+        'name':'Government Whip',
+        'code':'GW',
+        'score':0,
+        'team':"Second Government",
+        'points':{},
+        'pressed':false,
+        'default_colour':'btn btn-primary'
+    },
+
+    'OL' : {
+        'name':'Opposition Leader',
+        'code':'OL',
+        'score':0,
+        'team':"First Opposition",
+        'points':{},
+        'pressed':false,
+        'default_colour':'btn btn-danger'
+    },
+
+    'DO' : {
+        'name':'Deputy Opposition Leader',
+        'code':'DO',
+        'score':0,
+        'team':"First Opposition",
+        'points':{},
+        'pressed':false,
+        'default_colour':'btn btn-danger'
+    },
+
+    'OB' : {
+        'name':'Member of Opposition Benches',
+        'code':'OB',
+        'score':0,
+        'team':"Second Opposition",
+        'points':{},
+        'pressed':false,
+        'default_colour':'btn btn-danger'
+    },
+
+    'OW' : {
+        'name':'Opposition Whip',
+        'code':'OW',
+        'score':0,
+        'team':"Second Opposition",
+        'points':{},
+        'pressed':false,
+        'default_colour':'btn btn-danger'
+    }
+}
+
+var input = [];
+
+String.prototype.format = function() { // thx https://coderwall.com/p/flonoa/simple-string-format-in-javascript
+    a = this;
+    let a;
+    for (k in arguments) {
+        a = a.replace("{" + k + "}", arguments[k])
+    }
+    return a
+}
+
 
 function openSpeakerMenu(speaker) {
+    var screen = document.getElementById(speaker)
 
-    prev[2] = !prev[2]
+    if (Speakers[speaker]['pressed']){ //highlighting unpressed button and de-highlighting pressed button
+        document.getElementById(speaker + '_button').className = Speakers[speaker]['default_colour'];
+        while(screen.firstChild)
+            screen.lastChild.remove()
 
-    try {
-        document.getElementById("Speaker_screen").remove();
-        document.getElementById(prev[0]).className = prev[1];
-    } catch (TypeError) { }
+        Speakers[speaker]['pressed'] = !(Speakers[speaker]['pressed']);
 
-    var screen = document.createElement("div");
-    screen.className = "flex-box-container-1";
-    screen.id = "Speaker_screen";
-
-    var speakername = speakers.get(speaker)[0]
-
-    var classname;
-
-    if ((speakername.split(' ')).includes("Opposition"))
-        classname = "btn btn-danger";
-    else classname = "btn btn-primary"
-
-    if (prev[2]) {
-        document.getElementById(speaker).className = classname
-        if(prev[0]==speaker)
-            return
+        return
     }
-
-    prev[0] = speaker
-    prev[1] = classname;
-    document.getElementById(prev[0]).className = "btn btn-success";//higlighting pressed button
+    else
+        document.getElementById(speaker + '_button').className = "btn btn-success";
+    Speakers[speaker]['pressed'] = !(Speakers[speaker]['pressed']);
 
     var point = document.createElement("button");
     var poi = document.createElement("button");
     var rebuttal = document.createElement("button");
 
-    point.id = "newPoint";
-    point.onclick = function () { newPoint(speaker) };
+    point.id = "new_point_"+speaker;
+    poi.id = "new_poi_" +speaker;
+    rebuttal.id = "new_rebuttal_"+speaker;
+
     point.innerHTML = "Make a new point";
-
-    poi.id = "newPOI";
-    poi.onclick = function () { getSpeakerInput(speaker) };
     poi.innerHTML = "Make a new POI";
-
-    rebuttal.id = "newPOI";
-    rebuttal.onclick = function () { test() };
     rebuttal.innerHTML = "Make a new Rebuttal";
+
+    point.onclick = function () { newPoint(speaker) };
+    poi.onclick = function () { getSpeakerInput(speaker) };
+    rebuttal.onclick = function () { test(speaker) };
+
+    var classname = Speakers[speaker]['default_colour'];
 
     point.className = classname;
     poi.className = classname;
     rebuttal.className = classname;
 
-    screen.appendChild(point)
-    screen.appendChild(poi)
-    screen.appendChild(rebuttal)
-    document.getElementById("Speaker_screen_bg").appendChild(screen);
+    screen.appendChild(point);
+    screen.appendChild(poi);
+    screen.appendChild(rebuttal);
+}
+
+function takeInput(parentNode, type, text, speaker){
+    var inputBox = document.createElement("input");
+    inputBox.id = speaker  + " input" + ": " +text;
+    inputBox.type = type;
+    inputBox.placeholder = text;
+
+    var inputButton = document.createElement("button");
+    inputButton.id = speaker  + " button" + ": " +text;
+    inputButton.className = Speakers[speaker]['default_colour']
+    inputButton.onclick = function (){
+        var box = document.getElementById(speaker  + " input" + ": " +text);
+        input.push(box.value); //adding the stuff to the list, and then taking it off later
+        box.remove();
+
+        var button = document.getElementById(speaker  + " button" + ": " +text);
+        button.remove();
+    };
+    parentNode.appendChild(inputBox);
+    parentNode.appendChild(inputButton);
 }
 
 function newPoint(maker) {
@@ -86,7 +176,7 @@ function newPoint(maker) {
 
 function newPOI(maker, speaker) {
     console.log("New POI from " + maker + " addressing " + speaker);
-    
+
     var score = Number(prompt("Enter a score from 1-10 for the POI", "0"));
 
     var response = Number(prompt("Enter a score from 1-10 for the response to the POI", "0"));
@@ -102,11 +192,11 @@ function getSpeakerInput(maker){
     try {
         document.getElementById("Action_screen").remove();
     } catch (TypeError) { }
-    
+
     var screen = document.createElement("div");
     screen.className = "flex-box-container-1";
     screen.id = "Action_screen";
-    
+
     var PM = document.createElement("button");PM.id="PM";
     var DP = document.createElement("button");DP.id="DP";
     var GB = document.createElement("button");GB.id="GB";
@@ -115,9 +205,9 @@ function getSpeakerInput(maker){
     var DO = document.createElement("button");DO.id="DO";
     var OB = document.createElement("button");OB.id="OB";
     var OW = document.createElement("button");OW.id="OW";
-    
+
     speaker_list = [PM,DP,GB,GW,OL,DO,OB,OW];
-    
+
     for (var index = 0; index < speaker_list.length; index++) {
         const element = speaker_list[index];
         element.className = "btn btn-success";
@@ -128,7 +218,7 @@ function getSpeakerInput(maker){
         }
         screen.appendChild(element)
     }
-    
+
     document.getElementById("POI_display").appendChild(screen);
 }
 
@@ -139,9 +229,14 @@ function printReport(){
         var points = iterator[1][2];
 
         console.log(name+": "+score);
-        
+
         points.forEach(element => {element.forEach(thing=>{console.log(thing)})});//Printing points and given scores
     }
 }
 
-function test() { console.log("ttest"); }
+function test(speaker) { console.log("todo"); }
+
+function t(){
+    var t = document.getElementById("input").value;
+    document.getElementById("outputtext").innerHTML = t;
+}
